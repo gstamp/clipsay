1e268eafe849ce379cc662248a2ae591f7851cca
Glen Stampoultzis <glen.stampoultzis@envato.com>
2021-08-11 11:48:04 +1000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,88 @@
+use clipboard_master::{CallbackResult, ClipboardHandler, Master};
+
+use clipboard::ClipboardContext;
+use clipboard::ClipboardProvider;
+use regex::Regex;
+use reqwest::blocking::Client;
+use rodio::OutputStream;
+
+use std::env;
+use std::io;
+use std::io::Seek;
+use std::io::SeekFrom;
+use std::io::{Cursor, Write};
+struct Handler;
+
+fn speak(clip: &str) -> () {
+    let client = Client::new();
+    let api_key =
+        env::var("AZURE_SPEECH_KEY").expect("Environment variable AZURE_SPEECH_KEY not present");
+    let token = client
+        .post("https://australiaeast.api.cognitive.microsoft.com/sts/v1.0/issuetoken")
+        .header("Ocp-Apim-Subscription-Key", api_key)
+        .header("Content-Length", 0)
+        .send()
+        .unwrap()
+        .text()
+        .unwrap();
+
+    let voice = "Microsoft Server Speech Text to Speech Voice (ja-JP, KeitaNeural)";
+    let body = format!("<speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'><voice name='{}'>{}</voice></speak>", voice, clip);
+
+    let mut bearer = "Bearer ".to_owned();
+    bearer.push_str(&token);
+
+    let req = client
+        .post("https://australiaeast.tts.speech.microsoft.com/cognitiveservices/v1")
+        .header(
+            "X-Microsoft-OutputFormat",
+            "audio-16khz-64kbitrate-mono-mp3",
+        )
+        .header("User-Agent", "australiaeast")
+        .header("Content-Type", "application/ssml+xml")
+        .header("Authorization", bearer)
+        .body(body);
+    let res = req.send().unwrap();
+
+    let sound_bytes = res.bytes().unwrap();
+
+    let mut c = Cursor::new(Vec::new());
+    c.write_all(&sound_bytes)
+        .expect("Unable to read sound file data");
+    c.seek(SeekFrom::Start(0))
+        .expect("Unable to seek to start of buffer");
+
+    let (_stream, stream_handle) =
+        OutputStream::try_default().expect("Unable to create output stream");
+    let sink = stream_handle.play_once(c).unwrap();
+    sink.sleep_until_end();
+}
+
+impl ClipboardHandler for Handler {
+    fn on_clipboard_change(&mut self) -> CallbackResult {
+        println!("Clipboard change happened!");
+
+        let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+
+        let text = ctx.get_contents().unwrap();
+
+        let re = Regex::new(r"[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f]")
+            .unwrap();
+
+        if re.is_match(&text) {
+            println!("{}", text);
+            speak(&text);
+        }
+
+        CallbackResult::Next
+    }
+
+    fn on_clipboard_error(&mut self, error: io::Error) -> CallbackResult {
+        eprintln!("Error: {}", error);
+        CallbackResult::Next
+    }
+}
+
+pub fn main() {
+    let _ = Master::new(Handler).run();
+}
